dce817492606577acff05d4ed3318f8bf068ecbf
Robert Quesada Borbon <robertllds07@gmail.com>
2025-10-24 19:50:44 -0600

    Login funcional
    Correo: admin@gmail.com
    Password: 12345

---
+++ PAW3CP1.Mvc/ServiceApi/UserService.cs
@@ -0,0 +1,37 @@
+﻿using System.Drawing;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+using PAW3CP1.Architecture;
+using PAW3CP1.Architecture.Providers;
+using PAW3CP1.Models.DTO;
+
+namespace PAW3CP1.Mvc.ServiceApi
+{
+    public interface IUserService
+    {
+        Task<UserDTO?> CheckLogin(string email);
+    }
+    public class UserService (IRestProvider restProvider) : IUserService
+    {
+        private readonly string _baseUrlMinimalApi = "https://localhost:7265/login";
+
+
+        public async Task<UserDTO?> CheckLogin(string email)
+        {
+            try
+            {
+                var url = $"{_baseUrlMinimalApi}?email={Uri.EscapeDataString(email)}";
+                var response = await restProvider.GetAsync(url, null);
+                return await JsonProvider.DeserializeAsync<UserDTO>(response);
+            }
+            catch (InvalidOperationException)
+            {
+                // Usuario no encontrado
+                return null;
+            }
+        }
+    }
+
+
+}
--- PAWCP1.MinimalApi/Program.cs
+++ PAWCP1.MinimalApi/Program.cs
@@ -11,7 +11,9 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 // Registrar repositorio y business en DI
 builder.Services.AddScoped<IRepositoryTask, RepositoryTask>();
+builder.Services.AddScoped<IRepositoryUser, RepositoryUser>();
 builder.Services.AddScoped<ITaskBusiness, TaskBusiness>();
+builder.Services.AddScoped<IUserBusiness, UserBusiness>();
 var app = builder.Build();
 
 //obtener todos los prodcutos
@@ -21,6 +23,20 @@ app.MapGet("/Task", async (ITaskBusiness taskBusiness) =>
 
 app.MapGet("/Task/complete", async (TaskDbContext db) =>
     await db.Tasks.Where(t => t.Id != 0).ToListAsync());
+
+app.MapGet("/login", async (string email, IUserBusiness userBusiness) =>
+{
+    try
+    {
+        var user = await userBusiness.ValidateLogin(email);
+        return Results.Ok(user);
+    }
+    catch (InvalidOperationException ex)
+    {
+        return Results.NotFound(new { message = ex.Message });
+    }
+});
+
 
 
 app.Run();
